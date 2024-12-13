60169c4d4a85450d41b887ca9638653e7b387da6
kelvin <kalvind95@gmail.com>
2024-12-13 04:51:20 +0300

    implement get leads, delete lead and add lead actions

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,16 +2,21 @@ import React, {Component, Fragment} from "react";
 import ReactDOM from 'react-dom'
 import Header from "./layout/header";
 import Dashboard from "./leads/Dashboard";
+import {Provider} from 'react-redux';
+import Store from "../store";
+import store from "../store";
 
 class App extends Component {
         render() {
             return (
+                <Provider store={store}>
                 <Fragment>
                      <Header/>
                     <div className="container">
                          <Dashboard/>
                     </div>
                 </Fragment>
+                    </Provider>
 
             )
         }
